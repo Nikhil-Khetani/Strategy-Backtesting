5d585929dd8b780b6bbde8c6c28c3c0184437141
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-06-06 02:37:15 +0100

    Added price updates

--- trading.py
+++ trading.py
@@ -1,5 +1,6 @@
 import pandas_datareader.data as web
 import datetime
+import pandas as pd
 
 
 
@@ -8,13 +9,29 @@ class Portfolio(object):
         super().__init__()
         self.cash = cash
         self.tickers = tickers
-        self.assets = [Asset(i, start, end) for i in tickers]
+        self.assets = {i:Asset(i, start, end) for i in tickers}
+
+    def update_prices(self,date):
+        for i in tickers:
+            self.assets[i].update_price(date)
+    
+    def rebalance(self, percent_cash, **kwargs):
+        self.value = cash +  sum([self.assets[i].holding*self.assets[i].price for i in self.tickers])
+        for key, weight in kwargs.items():
+            self.assets[key].holding = self.value*weight/(100*self.assets[key].price)
+
+
 
 class Asset():
     def __init__(self, ticker, start, end):
         super().__init__()
         self.holding = 0
         self.data = web.DataReader(ticker, 'yahoo', start, end).reset_index(level=0)
+    
+    def update_price(self, date):
+
+        return
+
 
 
 
@@ -24,12 +41,19 @@ class Strategy():
         super().__init__()
         self.port = portfolio
 
+    def trading_strategy(self):
+        self.port.rebalance()
+        return
+
+    def backtest(self, start, end):
+        return
+
 
 if __name__ == '__main__':
     start = datetime.datetime(2011, 1, 1)
     end = datetime.datetime(2021, 1, 1)
     tickers = ['GBPJPY=X']
-
     port = Portfolio(cash=10000, tickers=tickers, start=start, end=end)
+    
 
     print(port.assets[0].data)
