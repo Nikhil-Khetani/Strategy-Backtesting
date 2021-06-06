6f971ec905e932f3a50270ff5e0e18cc3a2abde0
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-06-06 15:43:08 +0100

    Successfully allocated portfolio

--- trading.py
+++ trading.py
@@ -1,6 +1,7 @@
 import pandas_datareader.data as web
 import datetime
 import pandas as pd
+import re
 
 
 
@@ -8,17 +9,35 @@ class Portfolio(object):
     def __init__(self, cash, tickers, start, end):
         super().__init__()
         self.cash = cash
-        self.tickers = tickers
-        self.assets = {i:Asset(i, start, end) for i in tickers}
+        self.tickers = []
+        self.assets = {re.sub('=', '', i):Asset(i, start, end) for i in tickers}
+        for i in tickers:
+            self.tickers.append(re.sub('=', '', i))
 
     def update_prices(self,date):
-        for i in tickers:
+        for i in self.tickers:
             self.assets[i].update_price(date)
     
     def rebalance(self, percent_cash, **kwargs):
-        self.value = cash +  sum([self.assets[i].holding*self.assets[i].price for i in self.tickers])
-        for key, weight in kwargs.items():
-            self.assets[key].holding = self.value*weight/(100*self.assets[key].price)
+        
+        self.value = self.cash +  sum([self.assets[i].holding*self.assets[i].price for i in self.tickers])
+
+        if percent_cash+sum(kwargs.values()) != 100:
+            print('Weights do not add up to 100')
+            return
+        for ticker in self.tickers:
+            self.cash = self.value
+            self.assets[ticker].holding = 0
+
+        for ticker, weight in kwargs.items():
+            self.assets[ticker].holding = self.value*weight/(100*self.assets[ticker].price)
+            self.cash -= self.assets[ticker].holding*self.assets[ticker].price
+    
+    def print_holdings(self):
+        print('Cash: {}'.format(self.cash))
+        for ticker in self.tickers:
+            print('{} holding: {}, price: £{}, value: £{}'.format(ticker, self.assets[ticker].holding, self.assets[ticker].price, self.assets[ticker].holding*self.assets[ticker].price))
+            
 
 
 
@@ -26,10 +45,10 @@ class Asset():
     def __init__(self, ticker, start, end):
         super().__init__()
         self.holding = 0
-        self.data = web.DataReader(ticker, 'yahoo', start, end).reset_index(level=0)
+        self.data = web.DataReader(ticker, 'yahoo', start, end) #.reset_index(level=0)
     
     def update_price(self, date):
-
+        self.price = self.data['Adj Close'][str(date)]
         return
 
 
@@ -52,8 +71,11 @@ class Strategy():
 if __name__ == '__main__':
     start = datetime.datetime(2011, 1, 1)
     end = datetime.datetime(2021, 1, 1)
-    tickers = ['GBPJPY=X']
+    tickers = ['JPYGBP=X', 'TSLA']
     port = Portfolio(cash=10000, tickers=tickers, start=start, end=end)
     
+    print(port.assets['JPYGBPX'].data)
+    port.update_prices(datetime.datetime(2020,1,31))
 
-    print(port.assets[0].data)
+    port.rebalance(percent_cash=75, JPYGBPX=-25, TSLA=50)
+    port.print_holdings()
