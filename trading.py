fc9f774a4c20946b1acc8eb499b58da483b819af
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-06-08 15:01:27 +0100

    Added trade dates for backtesting

--- trading.py
+++ trading.py
@@ -13,6 +13,9 @@ class Portfolio(object):
         self.assets = {re.sub('=', '', i):Asset(i, start, end) for i in tickers}
         for i in tickers:
             self.tickers.append(re.sub('=', '', i))
+        self.tradeable_dates = list(pd.date_range(start,end))
+        for i in self.tickers:
+            self.tradeable_dates = list(set(self.tradeable_dates).intersection(set(self.assets[i].data.index)))
 
     def update_prices(self,date):
         for i in self.tickers:
@@ -34,12 +37,11 @@ class Portfolio(object):
             self.cash -= self.assets[ticker].holding*self.assets[ticker].price
     
     def print_holdings(self):
+        print('Total Value: {}'.format(self.cash +  sum([self.assets[i].holding*self.assets[i].price for i in self.tickers])))
         print('Cash: {}'.format(self.cash))
         for ticker in self.tickers:
             print('{} holding: {}, price: £{}, value: £{}'.format(ticker, self.assets[ticker].holding, self.assets[ticker].price, self.assets[ticker].holding*self.assets[ticker].price))
             
-
-
 
 class Asset():
     def __init__(self, ticker, start, end):
@@ -54,28 +56,38 @@ class Asset():
 
 
 
-
 class Strategy():
     def __init__(self, portfolio):
         super().__init__()
         self.port = portfolio
 
     def trading_strategy(self):
-        self.port.rebalance()
+        
+        self.port.rebalance(percent_cash=75, JPYGBPX=-25, TSLA=50)
+
         return
 
     def backtest(self, start, end):
+        for date in self.port.tradeable_dates:
+            if date>start and date<end:
+                print(date)
+                
         return
+
 
 
 if __name__ == '__main__':
     start = datetime.datetime(2011, 1, 1)
     end = datetime.datetime(2021, 1, 1)
     tickers = ['JPYGBP=X', 'TSLA']
+
     port = Portfolio(cash=10000, tickers=tickers, start=start, end=end)
     
     print(port.assets['JPYGBPX'].data)
     port.update_prices(datetime.datetime(2020,1,31))
 
     port.rebalance(percent_cash=75, JPYGBPX=-25, TSLA=50)
+    port.update_prices(datetime.datetime(2020,2,28))
     port.print_holdings()
+
+
