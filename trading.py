82d3bc779fa49a35735342b7de8cc46ee17a2178
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-06-08 17:19:38 +0100

    Fixed Backtest

--- trading.py
+++ trading.py
@@ -2,7 +2,7 @@ import pandas_datareader.data as web
 import datetime
 import pandas as pd
 import re
-
+import matplotlib.pyplot as plt
 
 
 class Portfolio(object):
@@ -16,24 +16,24 @@ class Portfolio(object):
         self.tradeable_dates = list(pd.date_range(start,end))
         for i in self.tickers:
             self.tradeable_dates = list(set(self.tradeable_dates).intersection(set(self.assets[i].data.index)))
+        self.tradeable_dates.sort()
 
     def update_prices(self,date):
         for i in self.tickers:
             self.assets[i].update_price(date)
     
-    def rebalance(self, percent_cash, **kwargs):
-        
-        self.value = self.cash +  sum([self.assets[i].holding*self.assets[i].price for i in self.tickers])
+    def rebalance(self, percent_cash=None, **kwargs):
 
-        if percent_cash+sum(kwargs.values()) != 100:
+        if int(percent_cash+sum(kwargs.values())) != 100:
             print('Weights do not add up to 100')
             return
+        val=self.value()
+        self.cash = val
         for ticker in self.tickers:
-            self.cash = self.value
             self.assets[ticker].holding = 0
 
         for ticker, weight in kwargs.items():
-            self.assets[ticker].holding = self.value*weight/(100*self.assets[ticker].price)
+            self.assets[ticker].holding = val*weight/(100*self.assets[ticker].price)
             self.cash -= self.assets[ticker].holding*self.assets[ticker].price
     
     def print_holdings(self):
@@ -41,7 +41,9 @@ class Portfolio(object):
         print('Cash: {}'.format(self.cash))
         for ticker in self.tickers:
             print('{} holding: {}, price: £{}, value: £{}'.format(ticker, self.assets[ticker].holding, self.assets[ticker].price, self.assets[ticker].holding*self.assets[ticker].price))
-            
+    
+    def value(self):
+        return self.cash +  sum([self.assets[i].holding*self.assets[i].price for i in self.tickers])
 
 class Asset():
     def __init__(self, ticker, start, end):
@@ -61,17 +63,48 @@ class Strategy():
         super().__init__()
         self.port = portfolio
 
-    def trading_strategy(self):
+    def trading_strategy(self, date):
+        self.port.update_prices(date)
+        weights = {}
+        cash_weight = 0
+        for i in self.port.tickers:
+            if self.port.assets[i].data['Adj Close'][date] > self.port.assets[i].data['Adj Close'].iloc[self.port.tradeable_dates.index(date)-10]:
+                weights[i] = 1
+            else:
+                weights[i] = 0.5
+                cash_weight += 0.5
         
-        self.port.rebalance(percent_cash=75, JPYGBPX=-25, TSLA=50)
-
-        return
+        normalisation_factor = 100/(sum(weights.values())+cash_weight)
+        cash_weight *= normalisation_factor
+        for k in weights:
+            weights[k] = weights[k]*normalisation_factor
+            
+        if True:
+            self.port.rebalance(percent_cash = cash_weight, **weights)
+            print('Trade')
+        return cash_weight, weights
 
     def backtest(self, start, end):
+        date_range=[]
+        port_value=[]
+        percent_change = [0]
+        
         for date in self.port.tradeable_dates:
             if date>start and date<end:
-                print(date)
-                
+                cash_weight, weights=self.trading_strategy(date)
+                date_range.append(date)
+                port_value.append(self.port.value())
+                print(cash_weight)
+                print(weights)
+
+        for i in range(len(port_value)-1):
+            percent_change.append((port_value[i+1]/port_value[i])-1)
+            
+
+        plt.scatter(date_range,port_value)
+        plt.show()
+        plt.scatter(date_range,percent_change)
+        plt.show()
         return
 
 
@@ -79,15 +112,14 @@ class Strategy():
 if __name__ == '__main__':
     start = datetime.datetime(2011, 1, 1)
     end = datetime.datetime(2021, 1, 1)
-    tickers = ['JPYGBP=X', 'TSLA']
+    tickers = ['SQQQ', 'TSLA']
 
     port = Portfolio(cash=10000, tickers=tickers, start=start, end=end)
     
-    print(port.assets['JPYGBPX'].data)
-    port.update_prices(datetime.datetime(2020,1,31))
 
-    port.rebalance(percent_cash=75, JPYGBPX=-25, TSLA=50)
-    port.update_prices(datetime.datetime(2020,2,28))
+
+    strat = Strategy(port)
+
+    strat.backtest(datetime.datetime(2019, 1, 1),datetime.datetime(2021, 1, 31))
     port.print_holdings()
-
 
